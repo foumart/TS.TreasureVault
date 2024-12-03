21aab0023ded8cf0fd326a9ee3defc7752be876e
foumart <inthewoodsbg@yahoo.com>
2024-12-03 12:07:30 +0200

    Add resize, make the app occupy the entire screen

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,36 @@ import { initDevtools } from '@pixi/devtools';
 const app = new Application();
 initDevtools({ app });
 
+// test graphics object
+const rect = new Graphics();
+
 // Initialize the application with options
 (async () => {
     await app.init({
-        width: 800,
-        height: 600,
+        width: window.innerWidth,
+        height: window.innerHeight,
         backgroundColor: 0x1099bb,
     });
 
     // Append the canvas to the DOM
     document.body.appendChild(app.canvas);
-    
-    const rect = new Graphics()
-        .rect(0, 0, 100, 100)
-        .fill({texture:Texture.WHITE, alpha:0.5, color:0xFF0000})
-        .rect(100, 0, 100, 100)
-        .fill({color:0xFFFF00, alpha:0.5});
 
-    // Add the graphics object to the PIXI application
+    // Add resize event listener
+    window.addEventListener('resize', resize);
+
+    // Add a test graphics object to the PIXI application
     app.stage.addChild(rect);
+
+    updateGraphics();
 })();
+
+function updateGraphics() {
+    rect.clear();
+    rect.rect(window.innerWidth / 2 - 50, window.innerHeight / 2 - 50, 100, 100)
+        .fill({texture:Texture.WHITE, alpha:0.5, color:0xFF0000})
+}
+
+function resize() {
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+    updateGraphics();
+};
